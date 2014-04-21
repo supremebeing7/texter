48ec16cfe47763fe02572388161acb27527c8f4e
Mark Lehman & Benjamin Paul Dunphy <student@epicodus.com>
2014-04-21 15:31:25 -0700

    Add Contacts Controller and New Contact view.

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,29 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = "Your contact was created!"
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+private
+
+  def contact_params
+    params.require(:contact).permit(:name, :phone, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Texter::Application.routes.draw do
   resources :messages, only: [:index, :create, :new, :show]
+  resources :contacts, only: [:index, :create, :new, :show]
+
   root to: 'messages#index'
   devise_for :users
 end
