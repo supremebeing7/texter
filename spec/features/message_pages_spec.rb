d6ea04ddc8a2f9710793f26103f75287ab49ed1d
Mark Lehman & Benjamin Paul Dunphy <student@epicodus.com>
2014-04-21 14:07:33 -0700

    Add VCR config and cassette library to message model and spec.

--- spec/features/message_pages_spec.rb
+++ spec/features/message_pages_spec.rb
@@ -4,8 +4,11 @@ describe Message do
   context 'sending' do
     it 'allows a user to send a text' do
       visit messages_path
-      click_link "Send a message"
-
+      click_link "New message"
+      page.should have_content 'New Message'
+      fill_in 'To', with: '8572641943'
+      fill_in 'From', with: '15005550006'
+      fill_in 'Body', with: Faker::Lorem.sentence
     end
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,8 +4,10 @@ describe Message, vcr: true do
   it { should validate_presence_of :body }
   it { should validate_presence_of :to }
   it { should validate_presence_of :from }
+
   it "doesn't save the message if twilio gives an error" do
-    message = Message.new(:body => 'hi', :to => '111111', :from => '15005550006')
-    message.save.should be_false
+    message = Message.new(:body => 'hi', :to => '11111111', :from => '15005550006')
+    message.save
+    message.errors.messages[:base].should eq ["The 'To' number 11111111 is not a valid phone number."]
   end
 end
