9bd6b7c53ff44fce19772a59b4257096c0ac78d0
Mark Lehman & Benjamin Paul Dunphy <student@epicodus.com>
2014-04-21 11:27:02 -0700

    Add form for message submit on messages/new.

--- app/models/message.rb
+++ app/models/message.rb
@@ -17,3 +17,13 @@ private
     ).execute
   end
 end
+
+#     response = RestClient::Request.new(
+#   :method => :post,
+#   :url => 'https://api.twilio.com/2010-04-01/Accounts/AC5a7ef1f080ae80ff763fd4fef562f77b/Messages.json',
+#   :user => 'AC5a7ef1f080ae80ff763fd4fef562f77b',
+#   :password => '78f2649b57b5130b96d89655240a2e90',
+#   :payload => {  :Body => 'Hello World',
+#               :From => '15005550006',
+#               :To => '13109076275' }
+# ).execute
