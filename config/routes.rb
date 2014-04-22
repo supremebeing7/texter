fd5fdc6afc0ca9e1ac5897ad1bdd755fd01728e9
Mark Lehman & Benjamin Paul Dunphy <student@epicodus.com>
2014-04-22 09:46:36 -0700

    Fix error message for text to non-valid phone number. Add Welcome page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Texter::Application.routes.draw do
   resources :messages, only: [:index, :create, :new, :show]
   resources :contacts, only: [:index, :create, :new, :show]
+  resources :welcome, only: [:index]
 
-  root to: 'messages#index'
+  root to: 'welcome#index'
   devise_for :users
 end
